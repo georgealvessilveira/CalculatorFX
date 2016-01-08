7d5c22de94ae024c1e77032db7f54501d2d5695a
George Alves da Silveira <georgealvessilveira@outlook.com>
2016-01-08 12:40:10 -0200

    ajeitando os botoes da interface

--- src/application/MainWindowController.java
+++ src/application/MainWindowController.java
@@ -1,9 +1,16 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class MainWindowController {
 	
+	@FXML Label display;
+	@FXML Button ac, conversion;
+	
 	private Main main;
-
+	
 	public MainWindowController setMain(Main main) {
 		this.main = main;
 		return this;
